3aff1b2a5eee2033a0fdeb42505f16bcc41a52b9
bopry <bopry6@gmail.com>
2025-08-28 22:51:02 +0300

    Add files via upload

---
+++ tracks.js
@@ -0,0 +1,5 @@
+const trackNamesArr = [
+    {trackName:"dj_osama_platina", artistName:'PryBoi', imgName:"https://i1.sndcdn.com/artworks-NZp7F8RhY27qdGgA-BRXLJw-t500x500.jpg"}, 
+    /*{trackName:"Розовое вино 2", artistName:'Yung Trappa', imgName:"https://i1.sndcdn.com/artworks-1MpP3kLOl01H6ddj-FQkmww-t500x500.jpg"}*/
+    {trackName:"ну", artistName:'Vlad Fomenko', imgName:"IMG_1290.jpeg"}
+];
